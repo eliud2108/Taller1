6f57a4f20ecf906e9a2b0077a0a0d3fa799ee212
eliud2108 <112783683+eliud2108@users.noreply.github.com>
2023-08-29 18:18:18 -0500

    Se documento tanto el primer como el segundo punto

--- primer_punto.py
+++ primer_punto.py
@@ -1,16 +1,16 @@
-def evaluar_expresion(expresion, x0, y0, h, n):
+def evaluar_expresion(expresion, x0, y0, h, n): #definimos la función que se encargará de seleccionar los caracteres permitidos y ejecutar la ecuación diferencial
     variables_permitidas = {'x', 'y'}
     operadores_permitidos = {'+', '-', '*', '/'}
     numeros_permitidos = set('0123456789 ')
     caracteres_permitidos = variables_permitidas | operadores_permitidos | numeros_permitidos | {'(', ')'}
     
-    if all(char in caracteres_permitidos for char in expresion):
+    if all(char in caracteres_permitidos for char in expresion): #se usa la built-in "all" para permitir la ejecución de la ecuación solo si se cumplen los requerimientos de los caracteres.
         vbles_remplazo = {'x': x0, 'y': y0}
         
         print(f"Iteración |    x    |    y    ")
         print("-" * 31)
         
-        for i in range(n + 1):
+        for i in range(n + 1): #la iteración va hasta el numero de pasos (n) digitado
             resultado = eval(expresion, vbles_remplazo)
             print(f"{i:^10} | {x0:^8.1f} | {y0:^8.3f}")
             
@@ -22,12 +22,13 @@ def evaluar_expresion(expresion, x0, y0, h, n):
     else:
         return "Expresión inválida"
 
-print (f"Este código resuelve EDOs por el método de euler")
+print(f"{ 'Este código resuelve EDOs por el método de euler'.center(50) }")
+
 x0 = float(input("Ingresa el valor inicial de x (x0): "))
 y0 = float(input("Ingresa el valor inicial de y (y0): "))
 h = float(input("Ingresa el tamaño del paso (h): "))
 n = int(input("Ingresa el número de pasos (n): "))
-while True:
+while True: #hasta que el usuario no ejecute los caracteres nesarios para la ecuación, no permite continuar con la función evaluar_expresion
     ecuacion = input("Ingresa una expresión válida (usando x, y), o 'salir' para terminar: ")
 
     if ecuacion.lower() == 'salir':
--- segundo_punto.py
+++ segundo_punto.py
@@ -1,25 +1,29 @@
 
-def cifrar_cesar(texto, clave):
+def cifrar_cesar(texto, clave): #función que se encarga de recorrer los caracteres de la palabra digitada para cifrar
     cifrado = ""
     for caracter in texto:
         if caracter.isalpha():
-            cifrado += chr(((ord(caracter) - ord('a' ) + clave) % 26) + ord('a'))
+            cifrado += chr(((ord(caracter) - ord('a' ) + clave) % 26) + ord('a')) 
+            """la primera parte Calcula el índice del carácter en el alfabeto, 
+            luego se agrega la clave para realizar el desplazamiento y 
+            luego se calcula el resultado módulo 26 para asegurarse de que el desplazamiento 
+            se mantenga dentro de las letras del alfabeto"""
         else:
             cifrado += caracter
     return cifrado
 
 def descifrar_cesar(texto_cifrado, clave):
-    return cifrar_cesar(texto_cifrado, -clave)
+    return cifrar_cesar(texto_cifrado, -clave) #se pone la clave negativa para generar el desplazamiento inverso y lograr recuperar el mensaje original
 
 def cifrar():
-    texto = input("Ingresa el texto a cifrar: ").lower()
+    texto = input("Ingresa el texto a cifrar: ").lower() #se recibe el texto a cifrar y se convierte a minusculas con .lower()
     clave = int(input("Ingrese el desplazamiento (un número entero): "))
     
     texto_cifrado = cifrar_cesar(texto, clave)
     print(f"Texto cifrado: {texto_cifrado}")
 
 def descifrar():
-    texto_cifrado = input("Ingresa el texto cifrado: ")
+    texto_cifrado = input("Ingresa el texto cifrado: ") #se recibe el texto a descifrar y se convierte a minusculas con .lower()
     clave = int(input("Ingrese el desplazamiento (un número entero): "))
     
     texto_descifrado = descifrar_cesar(texto_cifrado, clave)
@@ -27,7 +31,7 @@ def descifrar():
 
 def funcion_cesar():
     
-    opcion = input("¿Quieres cifrar (C) o descifrar (D)? ").lower()
+    opcion = input("¿Quieres cifrar (C) o descifrar (D)? ").lower() #Permite escoger de entrada si se quiere cifrar o descifrar
     
     if opcion == 'c':
         cifrar()
